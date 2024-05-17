5f3197705d183f56b1e2d6edf25f18ed7fcdec93
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2024-05-18 01:33:39 +0700

    fix:  fix cart

--- src/features/cart/cart.service.ts
+++ src/features/cart/cart.service.ts
@@ -65,8 +65,6 @@ export class CartService {
                 user: user,
                 id: cartItem.productId,
             });
-            
-            
 
             const cartExists = await this.prismaService.cart.findFirst({
                 where: {
@@ -75,15 +73,18 @@ export class CartService {
                 },
             });
 
-            const cartItemExists = await this.prismaService.cartItem.findFirst({
-                where: {
-                    cart_id: cartExists.id,
-                    product_id: cartItem.productId,
-                },
-            });
-
-            if (product.quantity < (cartItem.quantity + cartItemExists.quantity)) {
-                throw new GrpcInvalidArgumentException('PRODUCT_NOT_ENOUGH');
+            if (cartExists !== null) {
+                const cartItemExists = await this.prismaService.cartItem.findFirst({
+                    where: {
+                        cart_id: cartExists.id,
+                        product_id: cartItem.productId,
+                    },
+                });
+                if (cartItemExists !== null) {
+                    if (product.quantity < cartItem.quantity + cartItemExists.quantity) {
+                        throw new GrpcInvalidArgumentException('PRODUCT_NOT_ENOUGH');
+                    }
+                }
             }
 
             // Check if cart already exists
